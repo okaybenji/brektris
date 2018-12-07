1e0e4853c40c3dc1868a92eb882a97761a84fec9
Benji Kay <okaybenji@gmail.com>
2018-12-07 16:03:51 -0600

    tweak camera position

--- 3d.js
+++ 3d.js
@@ -5,11 +5,10 @@ const main = $('main');
 /** Set up Three.js **/
 // First we initialize the scene and our camera
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 45, main.offsetWidth / main.offsetHeight, 0.1, 10000 );
+const camera = new THREE.PerspectiveCamera( 44.5, main.offsetWidth / main.offsetHeight, 0.1, 10000 );
 camera.position.z = 3000;
-camera.position.y = -2250;
-camera.position.x = 570;
-camera.rotation.x = 0.25;
+camera.position.y = -1220;
+camera.position.x = 563;
 
 // We create the WebGL renderer and add it to the document
 const renderer = new THREE.WebGLRenderer();
@@ -72,10 +71,6 @@ let p;
 
 /** Animate the scene. **/
 const render = () => {
-  // Move the camera around for fun.
-  // camera.rotation.x = Math.sin(Date.now() / 2000) / 2;
-  // camera.position.y = -(Math.sin(Date.now() / 2000) * 1000) - 1000;
-
   // Get the Phaser scene.
   p = game.activeScene;
 
